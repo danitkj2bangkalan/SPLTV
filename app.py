dacc066a5040572b7150a9b5a8b1cc9162d25ee7
mrfirdaus <mrfirdaus123@gmail.com>
2022-01-03 18:37:18 +0700

    menambahkan proyek amin

--- app.py
+++ app.py
@@ -3,9 +3,48 @@ from flask_wtf import FlaskForm
 # membuat instance flask
 app = Flask(__name__)
 #membuat route decorator
-@app.route('/')
-def index():
-    return "tes"
+@app.route("/")
+def home():
+	return render_template("home.html")
+
+@app.get("/materi")
+def materi():
+	return render_template("materi.html")
+
+@app.get("/video")
+def video():
+	return render_template("video.html")
+
+@app.get("/tugas")
+def tugas():
+	return render_template("tugas.html")
+
+@app.get("/quiz")
+def quiz():
+	return render_template("quiz.html")
+
+@app.get("/absen")
+def absen():
+	return render_template("absen.html")
+
+@app.get("/referensi")
+def referensi():
+	return render_template("referensi.html")
+
+@app.get("/pengaturan")
+def pengaturan():
+	return render_template("pengaturan.html")
+
+@app.get("/daftar")
+def daftar():
+	return render_template("daftar.html")
+
+@app.get("/lupasandi")
+def lupasandi():
+	return render_template("lupasandi.html")
+
+
+
 #membuat custom error pages
 # Invalid URL
 @app.errorhandler(404)
