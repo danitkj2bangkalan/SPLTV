536a8657ae12ffd7d9ce105603a5b9c82c314eae
mrfirdaus <mrfirdaus123@gmail.com>
2022-01-03 20:51:26 +0700

    menambahkan gambar dan input form

--- app.py
+++ app.py
@@ -1,49 +1,70 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
-from flask_wtf import FlaskForm
+from webforms import LoginForm
 # membuat instance flask
 app = Flask(__name__)
-#membuat route decorator
-@app.route("/")
-def home():
-	return render_template("home.html")
+# untuk individual page
+app.config['SECRET_KEY'] = "sahur amin"
 
-@app.get("/materi")
+
+@app.route("/materi")
 def materi():
 	return render_template("materi.html")
 
-@app.get("/video")
+@app.route("/video")
 def video():
 	return render_template("video.html")
 
-@app.get("/tugas")
+@app.route("/tugas")
 def tugas():
 	return render_template("tugas.html")
 
-@app.get("/quiz")
+@app.route("/quiz")
 def quiz():
 	return render_template("quiz.html")
 
-@app.get("/absen")
+@app.route("/absen")
 def absen():
 	return render_template("absen.html")
 
-@app.get("/referensi")
+@app.route("/referensi")
 def referensi():
 	return render_template("referensi.html")
 
-@app.get("/pengaturan")
+@app.route("/pengaturan")
 def pengaturan():
 	return render_template("pengaturan.html")
 
-@app.get("/daftar")
+@app.route("/daftar")
 def daftar():
 	return render_template("daftar.html")
 
-@app.get("/lupasandi")
+@app.route("/lupasandi")
 def lupasandi():
 	return render_template("lupasandi.html")
 
+@app.route("/guru")
+def guru():
+	return render_template("guru.html")
 
+@app.route('/login',methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	username = None
+	password = None
+	if form.validate_on_submit():
+		username = form.username.data
+		password = form.password.data
+		print(username,password)
+		if username == 'amin' and password == 'amin':
+			flash("Sudah bisa masuk min")
+			return redirect(url_for('guru'))
+
+	return (form)
+
+@app.route("/")
+def home():
+	form = login()
+	return render_template("home.html", form=form)
 
 #membuat custom error pages
 # Invalid URL
---
+++ webforms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.widgets import TextArea
+# from flask_ckeditor import CKEditorField
+
+# Create Login Form
+class LoginForm(FlaskForm):
+	username = StringField("Username", validators=[DataRequired()])
+	password = PasswordField("Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
