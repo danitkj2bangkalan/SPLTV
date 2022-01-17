62110228b52d35b9110d71f96f435f267524e350
mrfirdaus <mrfirdaus123@gmail.com>
2022-01-17 11:26:17 +0700

    menghilangkan spalshscreen

--- static/js/splashscreen.js
+++ static/js/splashscreen.js
@@ -1,8 +1,8 @@
-const splash = document.querySelector('.splash');
+// const splash = document.querySelector('.splash');
 
-document.addEventListener('DOMContentLoaded', (e)=>{
-    setTimeout(()=>{
-        splash.classList.add('display-none');
-    }, 2000);
-})
+// document.addEventListener('DOMContentLoaded', (e)=>{
+//     setTimeout(()=>{
+//         splash.classList.add('display-none');
+//     }, 2000);
+// })
 
